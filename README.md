a49b2f189cd68d4b144e51be5df0e4bd186b6edc
coseyo <phpseyo@qq.com>
2016-01-25 21:06:03 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# beeconfig
+beeconfig is used to load config file under conf for beego
+
+For example, your app conf structure just like this:
+```
+conf/prod/a.ini
+conf/dev/a.ini
+```
+
+You want to load the specific file by runmode, so you just need this for solve this problem.
+
---
+++ config.go
@@ -0,0 +1,54 @@
+// Package beeconfig is used to load config file under conf for beego
+package beeconfig
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/config"
+)
+
+const defaultAdapter = "json"
+
+var configMap map[string]config.Configer
+
+// Load return Configer
+func Load(configName string, adapterName ...string) (cf config.Configer, err error) {
+
+	adapter := defaultAdapter
+	if len(adapterName) > 0 {
+		adapter = adapterName[0]
+	}
+
+	adapterFile := fmt.Sprintf("%s.%s", configName, adapter)
+	if c, ok := configMap[adapterFile]; ok {
+		cf = c
+		return
+	}
+
+	// beego 1.6 support beego.BConfig.RunMode
+	configFile := filepath.Join("conf", beego.AppConfig.String("RunMode"), adapterFile)
+
+	if !fileExist(configFile) {
+		configFile = filepath.Join("conf", adapterFile)
+		if !fileExist(configFile) {
+			err = errors.New("file not exist:" + configFile)
+			return
+		}
+	}
+
+	cf, err = config.NewConfig(adapter, configFile)
+	if err != nil {
+		configMap[adapterFile] = cf
+	}
+
+	return
+}
+
+func fileExist(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil || os.IsExist(err)
+}
